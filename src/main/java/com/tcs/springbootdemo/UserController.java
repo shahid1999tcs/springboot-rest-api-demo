03df96a718624c5735423e6389301533dce37dc1
shahid1999tcs <hellohbk29@gmail.com>
2021-08-25 06:30:44 +0000

    Added Exception resolver to send conditional status code

--- src/main/java/com/tcs/springbootdemo/UserController.java
+++ src/main/java/com/tcs/springbootdemo/UserController.java
@@ -1,7 +1,13 @@
 package com.tcs.springbootdemo;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +18,34 @@ public class UserController {
 	@Autowired
 	IUserService userservice;
 	
-	@GetMapping("/getuser")
-	public String getuser() {
+	@Autowired
+	IUserRepository repo;
+	
+	@GetMapping("/user")
+	public Iterable<User> getuser() {
 		System.out.println("Hello");
-		return "Hello";
+		return userservice.getAllUsers();
 	}
 	
 	@PostMapping("/user")
 	public void saveuser(@RequestBody User user) {
-		userservice.save(user);
+		//userservice.save(user);
+		repo.save(user);
 		System.out.println(user.getFirstName());
 		
+	}
+	
+	@ExceptionHandler(value = UserNotFoundException.class)
+	   public ResponseEntity<User> exception(UserNotFoundException userNotFoundException) {
+	      return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+	   }
+	
+	@GetMapping("/user/{id}")
+	private Optional<User> getUser(@PathVariable("id") Integer id) {
+		Optional<User> user = repo.findById(id);
+		if (!user.isPresent()) {
+			throw new UserNotFoundException("user does not exist");
+		}
+		return user;
 	}
 }
